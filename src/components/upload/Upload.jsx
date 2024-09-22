1043bfbb777e49ed2b348376db4a7433ab187a0a
Thamarai-selvan-G <thamaraiselvan878@gmail.com>
2024-09-22 11:07:25 +0530

    version 1

--- src/components/upload/Upload.jsx
+++ src/components/upload/Upload.jsx
@@ -5,24 +5,31 @@ import { SlClose } from "react-icons/sl";
 import { GrFormClose } from "react-icons/gr";
 import { VscCheck } from "react-icons/vsc";
 import { FiUpload } from "react-icons/fi";
+import { IoCaretBackCircle } from "react-icons/io5";
+import { IoCaretForwardCircle } from "react-icons/io5";
 import { useDispatch, useSelector } from "react-redux";
 import { myReducers } from "../../store/Store";
 import { useState } from "react";
 import { useRef } from "react";
 
 const Upload = () => {
+
   let userDatas = useSelector(function (data) {
     return data.user;
   });
+
   let dispatch = useDispatch();
+
   let [selectedUser, setSelectedUser] = useState(null);
   let [fileCss, setFileCss] = useState(false);
   let [dragActive, setDragActive] = useState(true);
   let [status, setstatus] = useState(true);
   let [drop, setDrop] = useState(true);
   let [selectedDocName, setSelectedDocName] = useState(null);
+  let [currentIndex, setCurrentIndex] = useState(0);
+   
 
-  function deleteUser(e) {
+  function deleteUser(e) {     // delete user name>>>
     dispatch(myReducers.deleteUser(e));
 
     if (selectedUser && selectedUser.name == e) {
@@ -31,7 +38,7 @@ const Upload = () => {
   }
   // console.log(userDatas);
 
-  function userDocs(val) {
+  function userDocs(val) {    // selecetd user name(value)>>>
     if (!val) {
       setSelectedUser(null);
     }
@@ -42,7 +49,7 @@ const Upload = () => {
   let [css, setCss] = useState(false);
   let nameDoc = useRef("");
 
-  function saveDocName() {
+  function saveDocName() {     // set the doc name in store >>>
     if (nameDoc.current.value && selectedUser) {
       dispatch(
         myReducers.setDocName({
@@ -62,13 +69,13 @@ const Upload = () => {
   }
   console.log(selectedUser);
 
-  let chooseRef = useRef("");
+  let chooseRef = useRef("");    // default choose styleing >>>
   function chooseFile() {
     if (chooseRef.current) {
       chooseRef.current.click();
     }
   }
-  function handleFileChange(e) {
+  function handleFileChange(e) {   // choose file handle >>>
     let file = e.target.files[0];
     if (file && selectedUser) {
       let fileUrl = URL.createObjectURL(file);
@@ -93,7 +100,7 @@ const Upload = () => {
     setDragActive(false);
   };
 
-  const handleDrop = (e) => {
+  const handleDrop = (e) => {          // drage colourm >>>
     e.preventDefault();
     e.stopPropagation();
     setDragActive(false);
@@ -109,42 +116,55 @@ const Upload = () => {
     }
   };
 
-  function cancelDoc() {
+  function cancelDoc() {             // remove import doc >>>
     let updatedUser = { ...selectedUser, docFile: [] };
     return setSelectedUser(updatedUser);
   }
+
+  function previousUser() {       // navigate btns >>>
+    if (currentIndex > 0) {
+      setCurrentIndex(currentIndex - 1);
+      userDocs(userDatas[currentIndex - 1]);
+    }
+  }
+  function nextUser() {
+    if (currentIndex < userDatas.length - 1) {
+      setCurrentIndex(currentIndex + 1);
+      userDocs(userDatas[currentIndex + 1]);
+    }
+  }
+
   return (
     <div>
       {css && (
         <div className="backBlack2">
- <div className="userInfo">
-          <div className="lineOne">
-            <h3>Add Document Name</h3>
-            <p onClick={() => setCss(!css)}>
-              <SlClose />
-            </p>
-          </div>
-          <div className="lineTwo">
-            <label>Name</label>
-            <input type="text" autoFocus ref={nameDoc} />
-          </div>
-          <div className="lineThree">
-            <button className="btn1" onClick={saveDocName}>
-              <p>
-                <VscCheck />
+          <div className="userInfo">
+            <div className="lineOne">
+              <h3>Add Document Name</h3>
+              <p onClick={() => setCss(!css)}>
+                <SlClose />
               </p>
-              Save
-            </button>
-            <button className="btn2" onClick={() => setCss(!css)}>
-              <p>
-                <GrFormClose />
-              </p>
-              Cancel
-            </button>
+            </div>
+            <div className="lineTwo">
+              <label>Name</label>
+              <input type="text" autoFocus ref={nameDoc} />
+            </div>
+            <div className="lineThree">
+              <button className="btn1" onClick={saveDocName}>
+                <p>
+                  <VscCheck />
+                </p>
+                Save
+              </button>
+              <button className="btn2" onClick={() => setCss(!css)}>
+                <p>
+                  <GrFormClose />
+                </p>
+                Cancel
+              </button>
+            </div>
           </div>
         </div>
-        </div>
-       
       )}
       <div className="userParent">
         <div className="nameRow">
@@ -262,8 +282,19 @@ const Upload = () => {
             )}
           </div>
         )}
+        <div className="navigateBtns">
+          <button className="navBtns" onClick={previousUser}>
+            {" "}
+            <IoCaretBackCircle />
+            Back
+          </button>
+          <button className="navBtns" onClick={nextUser}>
+            {" "}
+            <IoCaretForwardCircle />
+            Next
+          </button>
+        </div>
       </div>
-      
     </div>
   );
 };
